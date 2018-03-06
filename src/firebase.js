f407c6e76e34c2d6198c25ce94bc4a627781bf47
darkroom51 <darkroom@wp.pl>
2018-03-06 18:55:42 +0100

    packages installed, firebase init

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const config = {
+    apiKey: "AIzaSyAQhF4AB0Gz41z-mlehova7aPhMINh1FS4",
+    authDomain: "wld-todolists.firebaseapp.com",
+    databaseURL: "https://wld-todolists.firebaseio.com",
+    projectId: "wld-todolists",
+    storageBucket: "wld-todolists.appspot.com",
+    messagingSenderId: "66439859490"
+};
+firebase.initializeApp(config);
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const database = firebase.database()
+export const auth = firebase.auth()
